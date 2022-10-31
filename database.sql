87152007a743b70e6aa3b9cf17d4a2134436638e
UriLP <dash9uri@gmail.com>
2022-10-30 21:28:34 -0600

    Add comandos_usados.txt & database.sql

--- database.sql
+++ database.sql
@@ -1,19 +1,6 @@
-CREATE DATABASE IF NOT EXISTS agenda_telefonica;
-USE agenda_telefonica;
-
-CREATE TABLE contacto (
-  id INT AUTO_INCREMENT,
-  name VARCHAR(255) NOT NULL,
-  apellido VARCHAR(255) NOT NULL,
-  numero VARCHAR(255) NOT NULL,
-  direccion VARCHAR(255) NOT NULL,
-  email VARCHAR(255) NOT NULL,
-  PRIMARY KEY (id)
-);
-
+-- # Datos de prueba
 INSERT INTO contacto (name, apellido, numero, direccion, email) VALUES
-  ('Andrea', 'Martinez', '967356823', 'Queretaro', 'an@mar.com'),
+  ('Andrea', 'Martinez', '967356823', 'Guanajuato', 'an@mar.com'),
   ('Uriel', 'Loaiza', '898246889', 'Tlaxcala', 'uri@loa.com'),
-  ('Juan', 'Perez', '124785057', 'Guanajuato', 'juan@perez.com');
-
+  ('Jared', 'Partida', '124785057', 'Puebla', 'jar@par.com');
 
